918675235708f430b2ccf552a61e2f0a1633732a
dimas-adi-kris <62425346+dimas-adi-kris@users.noreply.github.com>
2023-08-19 20:39:42 +0700

    update: register admin. only once

--- app/controllers/HomeController.py
+++ app/controllers/HomeController.py
@@ -1,5 +1,5 @@
+from flask import render_template, request, Blueprint, session, redirect
 
-from flask import render_template, request, Blueprint, session,redirect
 # from app.models.user import User
 import app.models.UserModel as User
 
@@ -7,39 +7,42 @@ from app.middleware.Authentication import authentication
 from werkzeug.security import generate_password_hash, check_password_hash
 
 
-home = Blueprint('home', __name__)
+home = Blueprint("home", __name__)
 
-auth = Blueprint('auth', __name__)
+auth = Blueprint("auth", __name__)
 
 
 @home.route("/", methods=["GET", "POST"])
-@home.route('/index', methods=["GET", "POST"])
+@home.route("/index", methods=["GET", "POST"])
 def index():
+    # User.registerAdmin()
     if authentication():
         return render_template("pages/home/index.html")
     else:
         return render_template("pages/auth/login.html")
 
-@auth.route('/login', methods=["GET", "POST"])
+
+@auth.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "GET":
         return render_template("pages/auth/login.html")
     else:
         username = request.form.get("login")
         password = request.form.get("password")
-        print(username,password)
-        autentikasi = User.check_password(username,password)
+        print(username, password)
+        autentikasi = User.check_password(username, password)
         if autentikasi:
             print(autentikasi)
-            session["username"] = autentikasi['username']
-            session["role"] = autentikasi['role']
+            session["username"] = autentikasi["username"]
+            session["role"] = autentikasi["role"]
             return redirect("/")
         else:
-            return render_template("pages/auth/login.html",message="Login failed",status="danger")
+            return render_template(
+                "pages/auth/login.html", message="Login failed", status="danger"
+            )
 
 
-
-@auth.route('/register', methods=["GET", "POST"])
+@auth.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "GET":
         return render_template("pages/auth/register.html")
@@ -47,14 +50,20 @@ def register():
         username = request.form.get("username")
         password = request.form.get("password")
         if User.check_username(username):
-            return render_template("pages/auth/register.html",message="Username already exists",status="danger")
+            return render_template(
+                "pages/auth/register.html",
+                message="Username already exists",
+                status="danger",
+            )
         else:
-            User.add_user(username,password)
+            User.add_user(username, password)
             session["username"] = username
-            return render_template("pages/auth/login.html",message="Register success",status="success")
-        
+            return render_template(
+                "pages/auth/login.html", message="Register success", status="success"
+            )
 
-@auth.route('/logout', methods=["GET", "POST"])
+
+@auth.route("/logout", methods=["GET", "POST"])
 def logout():
     session.clear()
     return redirect("/")
--- app/models/UserModel.py
+++ app/models/UserModel.py
@@ -2,6 +2,7 @@ from app.db.firestore import db
 from ordereduuid import OrderedUUID
 from werkzeug.security import generate_password_hash, check_password_hash
 
+
 def get_all_data():
     users_ref = db.collection("users")
     docs = users_ref.stream()
@@ -13,29 +14,37 @@ def get_all_data():
         list_users.append(data)
     return list_users
 
+
 def get_data_by_id(id):
     data = db.collection("users").document(id).get().to_dict()
     data["id"] = id
     del data["password"]
     return data
 
-def add_user(username,password,role):
+
+def add_user(username, password, role):
     id = str(OrderedUUID())
     doc_ref = db.collection("users").document(id)
-    doc_ref.set({"username": username, "password": generate_password_hash(password), "role": role})
+    doc_ref.set(
+        {
+            "username": username,
+            "password": generate_password_hash(password),
+            "role": role,
+        }
+    )
     return {
-        "id":id,
-        "username":username,
+        "id": id,
+        "username": username,
     }
 
-def update_data(id,username,password,role):
+
+def update_data(id, username, password, role):
     doc_ref = db.collection("users").document(id)
-    doc_ref.set({"username": username, "password": generate_password_hash(password),role:role})
-    return {
-        "id":id,
-        "username":username,
-        "role":role
-    }
+    doc_ref.set(
+        {"username": username, "password": generate_password_hash(password), role: role}
+    )
+    return {"id": id, "username": username, "role": role}
+
 
 def delete_data(id):
     db.collection("users").document(id).delete()
@@ -44,7 +53,7 @@ def delete_data(id):
 
 def get_data_by_username(username):
     users_ref = db.collection("users")
-    query = users_ref.where(u'username', u'==', username)
+    query = users_ref.where("username", "==", username)
     docs = query.stream()
     list_users = []
     for doc in docs:
@@ -54,23 +63,41 @@ def get_data_by_username(username):
         list_users.append(data)
     return list_users
 
+
 def check_username(username):
     users_ref = db.collection("users")
-    query = users_ref.where(u'username', u'==', username)
+    query = users_ref.where("username", "==", username)
     docs = query.stream()
     for doc in docs:
         data = doc.to_dict()
         return True
     return False
 
-def check_password(username,password):
+
+def check_password(username, password):
     users_ref = db.collection("users")
-    query = users_ref.where(u'username', u'==', username)
+    query = users_ref.where("username", "==", username)
     docs = query.stream()
     for doc in docs:
         data = doc.to_dict()
-        if check_password_hash(data["password"],password):
+        if check_password_hash(data["password"], password):
             data["id"] = doc.id
             del data["password"]
             return data
     return False
+
+
+def registerAdmin():
+    id = str(OrderedUUID())
+    doc_ref = db.collection("users").document(id)
+    doc_ref.set(
+        {
+            "username": "admin",
+            "password": generate_password_hash("admin"),
+            "role": "admin",
+        }
+    )
+    return {
+        "id": id,
+        "username": "admin",
+    }
